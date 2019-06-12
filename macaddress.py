37aa1952d2bce16800510451f83d5c4584f96be1
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2019-06-12 15:53:31 +0530

    Update macaddress.py

--- macaddress.py
+++ macaddress.py
@@ -1,15 +1,10 @@
 import urllib.request
 import json
 import codecs
-url = ''
+url = 'https://api.macaddress.io/v1?apiKey=at_Jkpyix2qvXMRA29casQxJ1nqioU8b&output=json&search=44:38:39:ff:ef:57'
 json_obj = urllib.request.urlopen(url)
-
 reader = codecs.getreader("utf-8")
-
 data = json.load(reader(json_obj))
-
 #print (data)
-
 print ("company Name is " +data['vendorDetails']['companyName']);
-
 print ("comapany Address is " +data['vendorDetails']['companyAddress']);
