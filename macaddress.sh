dedcda9f7baf352a3a464fff9d24a427aae98709
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2019-06-13 22:36:34 +0530

    Create macaddress.sh

---
+++ macaddress.sh
@@ -0,0 +1,6 @@
+API_KEY=$1
+MAC_ADDRESS=$2
+#Build the Image using  Dockerfile
+docker image  build -t mac_addr .
+#Run the container and pass API_KEY and MAC_ADDRESS as arguments
+docker container run  mac_addr $API_KEY $MAC_ADDRESS
