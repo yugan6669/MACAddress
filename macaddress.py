6f0092425f5d51444e198be048b5007c64f0ce8e
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2019-06-13 18:27:53 +0530

    Update macaddress.py

--- macaddress.py
+++ macaddress.py
@@ -9,5 +9,4 @@ url = 'https://api.macaddress.io/v1?apiKey='+API_KEY+'&output=json&search='+MAC_
 json_obj = urllib.request.urlopen(url)
 reader = codecs.getreader("utf-8")
 data = json.load(reader(json_obj))
-print ("company Name is " +data['vendorDetails']['companyName']);
-print ("comapany Address is " +data['vendorDetails']['companyAddress'])
+print ("company Name is "+data['vendorDetails']['companyName']+ 'MAC Address is'+MAC_ADDRESS  );
